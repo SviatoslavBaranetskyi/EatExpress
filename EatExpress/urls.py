3fb4f853446b401835fa032487df82f98923c2ce
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-02-16 13:09:53 +0200

    initial commit

--- EatExpress/urls.py
+++ EatExpress/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/auth/', include('auth_app.urls')),
+    path('api/v1/restaurant/', include('restaurant_app.urls')),
+    path('api/v1/delivery/', include('delivery_app.urls')),
+    path('api/v1/payment/', include('payment_app.urls')),
 ]
