620c54b6bb89873f75cc393723e2ae4251b29e50
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-02-19 17:51:25 +0200

    added views for login, logout, register, profile

--- profile_app/views.py
+++ profile_app/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .models import Profile
+from .serializers import SignUpSerializer, ProfileSerializer
+
+
+class SignUpView(APIView):
+    def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()  # Saving a new user and user profile
+            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SignInView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)  # or HttpResponseRedirect('/')
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class ProfileView(APIView):
+    def get(self, request, slug):
+        user = get_object_or_404(User, username=slug)
+        profile = Profile.objects.get(user=user)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class SignOutView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({'message': 'Successfully logged out'}, status=status.HTTP_200_OK)  # or HttpResponseRedirect
