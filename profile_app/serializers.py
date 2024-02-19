b847da5af45630e8faf019d275f426639e11ef25
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-02-19 17:49:03 +0200

    added SignUp and Profile serializers

---
+++ profile_app/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+class SignUpSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    email = serializers.EmailField(max_length=255)
+    password = serializers.CharField()
+    confirm_password = serializers.CharField()
+    first_name = serializers.CharField(max_length=255)
+    last_name = serializers.CharField(max_length=255)
+    phone_number = serializers.CharField(max_length=15)
+    address = serializers.CharField(max_length=255)
+
+    def validate(self, data):
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+        if password != confirm_password:
+            raise serializers.ValidationError("Passwords do not match")
+        return data
+
+    def create(self, validated_data):
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
+        first_name = validated_data['first_name']
+        last_name = validated_data['last_name']
+        phone_number = validated_data['phone_number']
+        address = validated_data['address']
+
+        # Create and save a new user
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+
+        # Create and save a user profile
+        profile = Profile.objects.create(user=user, first_name=first_name, last_name=last_name,
+                                         phone_number=phone_number, address=address)
+        return user
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(source='user.email')
+
+    class Meta:
+        model = Profile
+        fields = ['first_name', 'last_name', 'phone_number', 'address', 'email']
