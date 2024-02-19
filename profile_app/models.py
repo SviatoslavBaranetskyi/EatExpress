848de04824ab47ad5dfe0a2a2cdba1d7bd0d2e3e
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-02-19 17:48:21 +0200

    added Profile model

--- profile_app/models.py
+++ profile_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
