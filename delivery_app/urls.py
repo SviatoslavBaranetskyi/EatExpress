f6462409ff7c4feb1167e668f1943f7334629a5e
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-03-07 14:57:59 +0200

    added courier auth and cart url

---
+++ delivery_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import CourierSignUpView, CourierSignInView, CourierSignOutView,  CourierProfileView, CartItemView, CartView
+
+urlpatterns = [
+    path('register/', CourierSignUpView.as_view(), name='courier-register'),
+    path('login/', CourierSignInView.as_view(), name='courier-login'),
+    path('logout/', CourierSignOutView.as_view(), name='courier-logout'),
+    path('profile/<slug:slug>', CourierProfileView.as_view(), name='courier-profile'),
+    path('cart/', CartView.as_view(), name='cart'),
+    path('cart-item/', CartItemView.as_view(), name='cart-item'),
+]
