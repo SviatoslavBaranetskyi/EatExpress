3b2fa4e08863da73bd697eb143a863893b02bccb
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-03-07 15:00:14 +0200

    refactor

--- profile_app/views.py
+++ profile_app/views.py
@@ -3,12 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth import authenticate, logout
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import Profile
 from .serializers import SignUpSerializer, ProfileSerializer
