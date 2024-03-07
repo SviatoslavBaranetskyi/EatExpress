6a2ff9da260ea981045c593de1dd8e78b6e2fbf6
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-03-07 14:56:45 +0200

    added Courier, CartItem, Cart models

--- delivery_app/models.py
+++ delivery_app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Courier(models.Model):
+    STATUS_CHOICES = [
+        ('Available', 'Available'),
+        ('Busy', 'Busy'),
+        ('Unavailable', 'Unavailable'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Available')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Cart(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('processing', 'Processing'),
+        ('on_delivery', 'On delivery'),
+        ('delivered', 'Delivered'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    address = models.CharField(max_length=255)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='active')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'CartUser - {self.user}, status - {self.status}'
+
+    @property
+    def items(self):
+        return self.cartitem_set.all()
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
+    dish_id = models.IntegerField()
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f'DishID - {self.dish_id}, quantity - {self.quantity}'
