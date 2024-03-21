73fc2f1a27b0792e0e447fd4f0ff6c2634720a48
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-03-21 11:29:29 +0200

    refactored

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import SignUpView, SignInView, SignOutView, ProfileView
 
 urlpatterns = [
-    path('user/register/', SignUpView.as_view(), name='user-register'),
-    path('user/login/', SignInView.as_view(), name='user-login'),
-    path('user/logout/', SignOutView.as_view(), name='user-logout'),
-    path('user/profile/<slug:slug>', ProfileView.as_view(), name='user-profile'),
+    path('register', SignUpView.as_view(), name='user-register'),
+    path('login', SignInView.as_view(), name='user-login'),
+    path('logout', SignOutView.as_view(), name='user-logout'),
+    path('profile/<slug:slug>', ProfileView.as_view(), name='user-profile'),
 ]
--- restaurant_app/urls.py
+++ restaurant_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import RestaurantMenuView
 
 urlpatterns = [
-    path('menu/', RestaurantMenuView.as_view(), name='categories'),
+    path('menu', RestaurantMenuView.as_view(), name='menu'),
 ]
