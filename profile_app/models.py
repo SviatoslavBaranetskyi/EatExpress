7629aae373ffb4ffb86eef6e25f515f9a846b097
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-03-21 11:28:23 +0200

    added the Courier model

--- profile_app/models.py
+++ profile_app/models.py
@@ -4,10 +4,23 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=15)
     address = models.CharField(max_length=255)
 
     def __str__(self):
         return self.user.username
+
+
+class Courier(models.Model):
+    STATUS_CHOICES = [
+        ('Available', 'Available'),
+        ('Busy', 'Busy'),
+        ('Unavailable', 'Unavailable'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Available')
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
