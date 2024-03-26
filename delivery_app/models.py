79d1e5c931b5c111be686205ed205296c93f535e
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-03-26 17:29:57 +0200

    added courier field

--- delivery_app/models.py
+++ delivery_app/models.py
@@ -1,7 +1,7 @@
-from django.utils import timezone
-
 from django.db import models
 from django.contrib.auth.models import User
+
+from profile_app.models import Courier
 
 
 class Order(models.Model):
@@ -26,6 +26,7 @@ class Order(models.Model):
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0, editable=False)
     delivery_address = models.CharField(max_length=255)
     payment_method = models.CharField(max_length=20, choices=PAYMENT_METHOD_CHOICES)
+    courier = models.ForeignKey(Courier, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'OrderUser - {self.user}, status - {self.status}'
