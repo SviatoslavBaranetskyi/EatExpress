124e6090b3a688d77de9cd483a9ea49130c895ea
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-03-26 17:32:29 +0200

    added courier field

--- delivery_app/serializers.py
+++ delivery_app/serializers.py
@@ -22,8 +22,19 @@ class OrderItemSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
     dishes = OrderItemSerializer(many=True, read_only=True)
     delivery_address = serializers.CharField(max_length=255, required=False)
+    courier = serializers.SerializerMethodField()
 
     class Meta:
         model = Order
-        fields = ['id', 'user', 'dishes', 'status', 'created_at', 'updated_at', 'total_price', 'delivery_address', 'payment_method']
-        read_only_fields = ['id', 'created_at', 'updated_at', 'total_price']
+        fields = ['id', 'user', 'dishes', 'status', 'created_at', 'updated_at', 'total_price', 'delivery_address',
+                  'payment_method', 'courier']
+        read_only_fields = ['id', 'created_at', 'updated_at', 'total_price', 'courier']
+
+    def get_courier(self, obj):
+        if obj.courier:
+            courier_data = {
+                'first_name': obj.courier.user.first_name,
+                'contact_info': obj.courier.phone_number,
+            }
+            return courier_data
+        return 'Search for a courier'
