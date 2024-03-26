8df515ffc9fbd71070736b22b7b221489dc239b2
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-03-26 17:36:54 +0200

    added path for ActiveOrdersView and AcceptOrderView

--- delivery_app/urls.py
+++ delivery_app/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('orders', OrderView.as_view(), name='orders'),
-    path('orders/<int:id>', OrderDetailView.as_view(), name='order-detail'),
+    path('orders/<int:order_id>', OrderDetailView.as_view(), name='order-detail'),
+    path('orders/active', ActiveOrdersView.as_view(), name='active-orders'),
+    path('orders/<int:order_id>/action', AcceptOrderView.as_view(), name='accept-order'),
 ]
