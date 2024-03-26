1b46617f10fed3efe784ff3ffa1127495ecada26
SkyDeaDy <svyatoslav.baranetskiy738@gmail.com>
2024-03-26 17:32:07 +0200

    updated IsCourier permission

--- delivery_app/permissions.py
+++ delivery_app/permissions.py
@@ -3,4 +3,4 @@ from rest_framework.permissions import BasePermission
 
 class IsCourier(BasePermission):
     def has_permission(self, request, view):
-        return request.user.groups.filter(name='Couriers').exists()
+        return request.user.groups.filter(name='CouriersGroup').exists() and request.user.courier.status == 'Available'
